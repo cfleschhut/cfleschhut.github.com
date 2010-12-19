e69c8ce89019ec1264b645fdd9120397d91ecc4a
Christian Fleschhut <weltrekordhalter@gmail.com>
2010-12-20 00:24:52 +0100

    yellow fade technique

--- code/bpAjax/ajaxtest.js
+++ code/bpAjax/ajaxtest.js
@@ -1,3 +1,37 @@
+function disappear(element, step) {
+	step = (typeof step != "undefined") ? step : 100;
+	if (timeoutID) {
+		clearTimeout(timeoutID);
+	}
+	element.style.opacity = step/100;
+	// console.log(step);
+	if (step <= 0) { return; }
+	var newstep = step - 5;
+	var timeoutID = setTimeout(function() {
+		disappear(element, newstep);
+	}, 40);
+}
+
+function fadeUp(element, red, green, blue) {
+	// console.log(red, green, blue);
+	// console.log(element.fade);
+	if (element.fade) {
+		clearTimeout(element.fade);
+	}
+	element.style.backgroundColor = "rgb(" + red + ", " + green + ", " + blue + ")";
+	if (red == 255 && green == 255 && blue == 255) {
+		return;
+	}
+	var newred = red + Math.ceil((255 - red)/10);
+	var newgreen = green + Math.ceil((255 - green)/10);
+	var newblue = blue + Math.ceil((255 - blue)/10);
+	var repeat = function() {
+		fadeUp(element, newred, newgreen, newblue);
+	};
+	element.fade = setTimeout(repeat, 40);
+}
+
+
 function getHTTPObject() {
 	var xhr = false;
 	if (window.XMLHttpRequest) {
