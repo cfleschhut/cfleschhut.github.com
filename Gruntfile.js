911aea876ed77baecbd41773006e20eeb8cf4bf1
Christian Fleschhut <weltrekordhalter@gmail.com>
2014-03-09 20:40:21 +0100

    grunt jshint + uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,28 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    jshint: {
+      files: ['js/main.js']
+    },
+
+    uglify: {
+      // debug: {
+      //   src: 'js/main.js',
+      //   dest: 'dist/main.js',
+      //   options: {
+      //     beautify: true
+      //   }
+      // },
+      minified: {
+        src: 'js/main.js',
+        dest: 'js/main.min.js',
+        options: {
+          mangle: true,
+          compress: true
+        }
+      }
+    },
+
     sass: {
       dist: {
         options: {
@@ -15,6 +37,9 @@ module.exports = function(grunt) {
     },
 
     watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint'],
+
       css: {
         files: ['css/scss/*.scss'],
         tasks: ['sass'],
@@ -25,9 +50,11 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['sass', 'watch']);
+  grunt.registerTask('default', ['jshint', 'uglify', 'sass']);
 
 };
